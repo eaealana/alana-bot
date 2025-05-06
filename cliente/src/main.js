6c6e3e278ae2be30ea187b3f4f7809305cf4bac5
Alana Ribeiro <ar.ribeiroalana@gmail.com>
2025-05-05 23:55:09 -0300

    add link

--- cliente/src/main.js
+++ cliente/src/main.js
@@ -77,7 +77,7 @@ const handleSubmit = async (e) => {
 
     // fetch data from server -> bot's response
 
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://alana-bot-83dd.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
